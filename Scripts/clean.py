5363472f47f7a515e451693176c0ad5b654bfa4e
Antoine Roucau <antoine.roucau@gmail.com>
2025-04-30 13:17:29 +0200

    Data clean

---
+++ Scripts/clean.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+
+def remove_incomplete_listings(input_file_path, output_file_path, critical_columns=None):
+    try:
+        df = pd.read_csv(input_file_path)
+        initial_shape = df.shape
+        df = df.replace('', np.nan)
+        df_cleaned = df.dropna()
+        final_shape = df_cleaned.shape
+        print(f"Lignes supprimées : {initial_shape[0] - final_shape[0]}")
+        df_cleaned.to_csv(output_file_path, index=False)
+        return df_cleaned
+        
+    except Exception as e:
+        print(f"{str(e)}")
+        return None
+
+if __name__ == "__main__":
+    input_file = "Data/Clean/train_partial.csv"  # CSV original
+    output_file = "Data/Clean/train_partial_cleanV2.csv"  # CSV nettoyé
+    
+    df_complete = remove_incomplete_listings(input_file, output_file)
---
+++ Scripts/selection.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+selected_columns = [
+    # Id et prix
+    'id', # 'log_price', pour le fichier train
+    # Appartement
+    'property_type', 'room_type', 'accommodates', 'bedrooms', 
+    'beds', 'bathrooms', 'amenities',
+    # Loc
+    'city', 'neighbourhood', 'latitude', 'longitude',
+    # Règles et politiques
+    'cancellation_policy', 'cleaning_fee', 'instant_bookable',
+    # Host
+    'host_since', 'host_identity_verified',
+    # Reviews
+    'number_of_reviews', 'review_scores_rating'
+]
+
+def select_columns(input_file_path, output_file_path, columns_to_keep):
+    try:
+        df = pd.read_csv(input_file_path)
+        df_selected = df[columns_to_keep]
+        df_selected.to_csv(output_file_path, index=False)
+        return df_selected
+        
+    except Exception as e:
+        print(f"{str(e)}")
+        return None
+
+if __name__ == "__main__":
+    input_file = "Data/Original/airbnb_test.csv"  # CSV original
+    output_file = "Data/Clean/test_partial.csv"  # CSV nettoyé
+    
+    df_cleaned = select_columns(input_file, output_file, selected_columns)
